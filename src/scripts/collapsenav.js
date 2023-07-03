ff29538da2881f8c55714bf7812fc470354ba59a
Franco <franangh.20@gmail.com>
2023-07-03 16:21:37 -0300

    cambios

---
+++ src/scripts/collapsenav.js
@@ -0,0 +1,1 @@
+function Menu(e){let s=document.querySelector("ul");"menu"===e.name?(e.name="close",s.classList.add("top-[80px]"),s.classList.add("opacity-100")):(e.name="menu",s.classList.remove("top-[80px]"),s.classList.remove("opacity-100"))}
